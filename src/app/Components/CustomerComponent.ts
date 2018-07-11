f348cdfe9b78e61e5a412132a209b61a6e027fb2
GIRI1982 <giridharan.arul@gmail.com>
2018-07-11 19:53:21 +0530

    Binding

--- src/app/Components/CustomerComponent.ts
+++ src/app/Components/CustomerComponent.ts
@@ -2,10 +2,32 @@ import { Component } from '@angular/core';
 import { Customer } from '../Model/Customer';
 
 @Component({
-    selector: 'app-root',
-    templateUrl : '../UI/Customer.html'
+    selector: 'app-customer',
+    templateUrl: '../UI/Customer.html',
+    styleUrls: ['../UI/Customer.css']
 })
 export class CustomerComponent {
     // binding logic
-    customerobj: Customer = new Customer();
+    currentCustomer: Customer = new Customer();
+    // customer collection
+    customers: Array<Customer> = new Array<Customer>();
+    Select(custselected: Customer) {
+        this.currentCustomer = Object.assign({}, custselected);
+    }
+    Clear() {
+        this.currentCustomer = new Customer();
+    }
+    Update() {
+        for (const cust of this.customers) {
+            if (cust.CustomerCode === this.currentCustomer.CustomerCode) {
+                cust.CustomerName = this.currentCustomer.CustomerName;
+                cust.CustomerAmount = this.currentCustomer.CustomerAmount;
+            }
+        }
+        this.currentCustomer = new Customer();
+    }
+    Add() {
+        this.customers.push(this.currentCustomer);
+        this.currentCustomer = new Customer();
+    }
 }
